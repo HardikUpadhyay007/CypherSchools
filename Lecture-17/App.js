51c43aeb2126a46bd330c25e8c7dd7cbaec5e99a
Hardik Upadhyay <119848774+HardikUpadhyay007@users.noreply.github.com>
2024-07-11 18:53:57 +0530

    Add files via upload

---
+++ Lecture-17/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+
+class App extends Component {
+    name = "CipherSchools";
+    render() {
+        return (
+            <>
+                <div>
+                    <h1>This is {this.name}!</h1>
+                </div>
+                <div>
+                    <p>This is paragraph and the name is: {this.name}</p>
+                </div>
+            </>
+        );
+    }
+}
+export default App;
